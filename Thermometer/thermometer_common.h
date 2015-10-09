681d6bf25b9fcc9ac304735bc7d287a05b354606
Giorgioggì <software@sukkology.net>
2015-10-10 00:22:34 +0200

    Updated copyright notice.

--- Thermometer/thermometer_common.h
+++ Thermometer/thermometer_common.h
@@ -1,20 +1,20 @@
 /***************************************************************************
- *   This file is part of SmartStrip.                                      *
+ *   This file is part of Sukkino.                                         *
  *                                                                         *
- *   Copyright (C) 2013 by SukkoPera                                       *
+ *   Copyright (C) 2013-2015 by SukkoPera                                  *
  *                                                                         *
- *   SmartStrip is free software: you can redistribute it and/or modify    *
+ *   Sukkino is free software: you can redistribute it and/or modify       *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation, either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   SmartStrip is distributed in the hope that it will be useful,         *
+ *   Sukkino is distributed in the hope that it will be useful,            *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with SmartStrip.  If not, see <http://www.gnu.org/licenses/>.   *
+ *   along with Sukkino.  If not, see <http://www.gnu.org/licenses/>.      *
  ***************************************************************************/
 
 #ifndef THERMOMETERCOMMON_H_
