1c75ea5a6743da54d8248c3f336f023e8956f0ec
SukkoPera <software@sukkology.net>
2016-05-22 10:55:04 +0200

    Update copyright notice

--- Webbino/WebServer_WIZ5100.cpp
+++ Webbino/WebServer_WIZ5100.cpp
@@ -3,18 +3,18 @@
  *                                                                         *
  *   Copyright (C) 2012-2016 by SukkoPera                                  *
  *                                                                         *
- *   SmartStrip is free software: you can redistribute it and/or modify    *
+ *   Webbino is free software: you can redistribute it and/or modify       *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation, either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   SmartStrip is distributed in the hope that it will be useful,         *
+ *   Webbino is distributed in the hope that it will be useful,            *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with SmartStrip.  If not, see <http://www.gnu.org/licenses/>.   *
+ *   along with Webbino. If not, see <http://www.gnu.org/licenses/>.       *
  ***************************************************************************/
 
 #include "WebServer_WIZ5100.h"
