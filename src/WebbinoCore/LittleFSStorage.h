d902cb23bbe5467b5d2ea1cd7951fbea72953bc6
SukkoPera <software@sukkology.net>
2021-01-15 13:53:11 +0100

    Update copyright notice

--- src/WebbinoCore/LittleFSStorage.h
+++ src/WebbinoCore/LittleFSStorage.h
@@ -1,7 +1,7 @@
  /***************************************************************************
  *   This file is part of Webbino                                          *
  *                                                                         *
- *   Copyright (C) 2012-2020 by SukkoPera                                  *
+ *   Copyright (C) 2012-2021 by SukkoPera                                  *
  *                                                                         *
  *   Webbino is free software: you can redistribute it and/or modify       *
  *   it under the terms of the GNU General Public License as published by  *
